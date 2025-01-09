7328dce73156dccc14e3666ea4b087eacbc91fd7
ramanpreet.singh <ramanpreet.singh@epita.fr>
2025-01-09 18:19:56 +0100

    feat: add builtin

---
+++ src/builtin/builtin_true_false.c
@@ -0,0 +1,29 @@
+#include "builtin.h"
+
+#include <stddef.h>
+
+// Builtin_TRUE
+int builtin_true(char **argv)
+{
+    if (argv != NULL)
+    {
+        if (argv[0] != NULL)
+        {
+            return 0;
+        }
+    }
+    return 0;
+}
+
+// Builtin_FALSE
+int builtin_false(char **argv)
+{
+    if (argv != NULL)
+    {
+        if (argv[0] != NULL)
+        {
+            return 1;
+        }
+    }
+    return 1;
+}
